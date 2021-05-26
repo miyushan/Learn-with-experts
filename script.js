4290e5b21047fff4e30a6bffd6ed728eb7cd54ea
miyushan <smiushan2015@gmail.com>
2021-05-27 01:14:53 +0530

    improve navbar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
     window.onscroll = function(){
-        if(document.body.scrollTop >= 170.063 || document.documentElement.scrollTop >= 170.063){
+        if(document.body.scrollTop >= 130 || document.documentElement.scrollTop >= 130){
             // console.log("Hii");
             outter.classList.add("scroll");
         } else {
